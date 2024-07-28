30b7710aa38b6dbde399c193f14abeb98b9d9208
Gaurav Prakash <202151056@iiitvadodara.ac.in>
2024-07-28 17:35:42 +0530

    corrected linting

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,31 +2,31 @@ import CloseButton from '@/components/CloseButton';
 import React from 'react';
 
 const AboutUsPage = () => (
-  <section className="relative px-32 p-8 bg-gradient-to-b from-white to-green-200">
-    <CloseButton  />
-    <h2 className="mt-8 text-center text-4xl mb-8 font-bold">About Us</h2>
-    <h3 className="text-2xl font-semibold mt-4">Welcome to Rido</h3>
-    <p>
+  <section className="relative p-8 px-32 bg-gradient-to-b from-white to-green-200">
+    <CloseButton />
+    <h2 className="mt-8 mb-8 text-center text-4xl font-bold">About Us</h2>
+    <h3 className="mt-4 text-2xl font-semibold">Welcome to Rido</h3>
+    <p className="mt-2">
       We believe that every journey should be a pleasant and memorable experience. Established in 2024, our mission has been to provide safe, reliable, and comfortable transportation solutions for our valued customers. We understand the importance of punctuality, convenience, and professionalism, which is why we go above and beyond to exceed your expectations.
     </p>
     
-    <h3 className="text-2xl font-semibold mt-4">Our Story</h3>
-    <p>
+    <h3 className="mt-4 text-2xl font-semibold">Our Story</h3>
+    <p className="mt-2">
       Rido was founded by Mr. Satyam Krishna with a vision to revolutionize the transportation industry. With years of experience and a passion for customer service, Satyam Krishna set out to create a cab rental service that offers more than just a ride. From humble beginnings, we have grown into a trusted name in the industry, known for our dedication to quality and excellence.
     </p>
     
-    <h3 className="text-2xl font-semibold mt-4">Our Fleet</h3>
-    <p>
-      Our diverse fleet of vehicles caters to all your transportation needs. Whether you're looking for a luxury sedan for a business trip, a spacious SUV for a family outing, or a convenient minivan for group travel, we have the perfect vehicle for you. Each vehicle is meticulously maintained to ensure a smooth and comfortable ride.
+    <h3 className="mt-4 text-2xl font-semibold">Our Fleet</h3>
+    <p className="mt-2">
+      Our diverse fleet of vehicles caters to all your transportation needs. Whether you&apos;re looking for a luxury sedan for a business trip, a spacious SUV for a family outing, or a convenient minivan for group travel, we have the perfect vehicle for you. Each vehicle is meticulously maintained to ensure a smooth and comfortable ride.
     </p>
     
-    <h3 className="text-2xl font-semibold mt-4">Our Team</h3>
-    <p>
+    <h3 className="mt-4 text-2xl font-semibold">Our Team</h3>
+    <p className="mt-2">
       Our team of professional drivers is the backbone of our service. Carefully selected for their experience, knowledge, and courteous demeanor, they are dedicated to providing you with a safe and enjoyable journey. Our support staff is always ready to assist you with any inquiries or special requests, ensuring a seamless experience from booking to drop-off.
     </p>
     
-    <h3 className="text-2xl font-semibold mt-4">Our Commitment</h3>
-    <p>
+    <h3 className="mt-4 text-2xl font-semibold">Our Commitment</h3>
+    <p className="mt-2">
       At Rido, we are committed to continuous improvement. We regularly seek feedback from our customers and use it to enhance our services. Our goal is to be your trusted transportation partner, delivering exceptional service every time you choose us.
     </p>
   </section>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const ContactUsPage = () => {
   return (
     <section className="p-8 px-32 bg-gradient-to-b from-white to-green-200">
-        <CloseButton/>
-    <h2 className="mt-8 text-center text-4xl mb-8 font-bold">Contact Us</h2>
+      <CloseButton />
+      <h2 className="mt-8 mb-8 text-center text-4xl font-bold">Contact Us</h2>
       <h2 className="text-2xl font-bold">Get in Touch</h2>
       <p className="mt-4 text-lg">
         At Rido, we value your feedback and are here to assist you with any questions or concerns. Whether you need help with a booking, have a suggestion, or require support, our dedicated customer service team is ready to provide you with prompt and professional assistance.
@@ -20,7 +20,7 @@ const ContactUsPage = () => {
 
       <div className="mt-6">
         <h3 className="text-xl font-semibold">Driver Support</h3>
-        <p>If you're a driver needing assistance or interested in joining our team, our driver support team is here to help.</p>
+        <p>If you&apos;re a driver needing assistance or interested in joining our team, our driver support team is here to help.</p>
         <p>Phone: 70-70-70-3855</p>
         <p>Email: <a href="mailto:drivers@ridocabs.com" className="text-blue-500">drivers@ridocabs.com</a></p>
       </div>
